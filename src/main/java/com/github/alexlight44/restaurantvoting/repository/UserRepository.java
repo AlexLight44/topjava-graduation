af018238b9136d5a23f19a466d63ae651102e1f7
Alex <easyfreedom44@gmail.com>
2026-09-18 12:53:04 +0300

    Cacheble User

--- src/main/java/com/github/alexlight44/restaurantvoting/repository/UserRepository.java
+++ src/main/java/com/github/alexlight44/restaurantvoting/repository/UserRepository.java
@@ -1,5 +1,6 @@
 package com.github.alexlight44.restaurantvoting.repository;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.transaction.annotation.Transactional;
 import com.github.alexlight44.restaurantvoting.model.User;
@@ -9,6 +10,6 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Integer> {
 
+    @Cacheable("users")
     Optional<User> findByEmailIgnoreCase(String email);
-
 }
